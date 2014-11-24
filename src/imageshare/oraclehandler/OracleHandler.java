43d3a5860a381eede08290d8148a04dda3cb26a7
Jessica Yuen <jyuen@ualberta.ca>
2014-11-23 19:58:36 -0700

    adding missing import

--- src/imageshare/oraclehandler/OracleHandler.java
+++ src/imageshare/oraclehandler/OracleHandler.java
@@ -18,6 +18,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
